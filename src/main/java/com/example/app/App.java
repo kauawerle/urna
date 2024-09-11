280f013ce5e445b87ee5de75e85ed9dcd7cf62cb
Kauã Werle <kauawerle19@gmail.com>
2024-09-11 20:38:27 -0300

    create package entities

--- src/main/java/com/example/app/App.java
+++ src/main/java/com/example/app/App.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UrnaVirtualApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UrnaVirtualApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
