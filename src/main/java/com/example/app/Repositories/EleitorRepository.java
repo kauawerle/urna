89f5b25f5ecc698a8c69ad7e5d41fc3979b1091a
wtfpac <kwellxz@gmail.com>
2024-09-12 19:50:43 -0300

    repositorios eleitor

---
+++ src/main/java/com/example/app/Repositories/EleitorRepository.java
@@ -0,0 +1,8 @@
+package com.example.app.Repositories;
+
+import com.example.app.Entities.Eleitor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EleitorRepository extends JpaRepository<Eleitor, Long> {
+
+}
