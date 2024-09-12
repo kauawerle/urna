302e6a45077f5730d85b031023fc24836e97e974
wtfpac <kwellxz@gmail.com>
2024-09-12 18:40:13 -0300

    att no eleitor

--- src/main/java/com/example/app/Entities/Eleitor.java
+++ src/main/java/com/example/app/Entities/Eleitor.java
@@ -1,4 +1,42 @@
 package com.example.app.Entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name= "t_eleitor")
 public class Eleitor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotBlank
+    private String cpf;
+
+    @NotBlank
+    private String profissao;
+
+    @NotBlank
+    private String celular;
+
+    @NotBlank
+    private String telefone;
+
+    @NotBlank
+    private String email;
+
+    private String status;
+
+
+
 }
