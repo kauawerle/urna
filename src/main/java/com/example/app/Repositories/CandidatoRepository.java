ad9b85c2ecad9971d0b8206b9f24a647d3332d5c
kauawerle <kauawerle19@gmail.com>
2024-09-12 20:04:48 -0300

    feat:implement new methods to repositories

--- src/main/java/com/example/app/Repositories/CandidatoRepository.java
+++ src/main/java/com/example/app/Repositories/CandidatoRepository.java
@@ -1,8 +1,15 @@
 package com.example.app.Repositories;
 
 import com.example.app.Entities.Candidato;
+import com.example.app.Enum.StatusCandidato;
+import com.example.app.Enum.StatusEleitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
+import java.util.List;
 
+public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
+    List<Candidato> findByPrefeitoName(String nomePrefeito, int cargo, StatusCandidato status);
+    List<Candidato> findByVereadorName(String nomeVereador, int cargo, StatusCandidato status);
+
+    void updateUserStatus(Long userId, StatusEleitor newStatus);
 }
--- src/main/java/com/example/app/Repositories/EleitorRepository.java
+++ src/main/java/com/example/app/Repositories/EleitorRepository.java
@@ -1,8 +1,9 @@
 package com.example.app.Repositories;
 
 import com.example.app.Entities.Eleitor;
+import com.example.app.Enum.StatusEleitor;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EleitorRepository extends JpaRepository<Eleitor, Long> {
-
+    void updateUserStatus(Long userId, StatusEleitor newStatus);
 }
