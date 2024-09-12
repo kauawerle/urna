e2e99a1ce572eb956f479fd2b236fad89f724514
Kauã Werle <kauawerle19@gmail.com>
2024-09-12 18:19:04 -0300

    feat implement atributtes

--- src/main/java/com/example/app/Entities/Candidato.java
+++ src/main/java/com/example/app/Entities/Candidato.java
@@ -1,6 +1,26 @@
 package com.example.app.Entities;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
+@Entity
 public class Candidato {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
 
+    @NotBlank
+    private String nome_completo;
+
+    @Pattern(regexp = "\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}", message = "CPF deve estar no formato 000.000.000-00")
+    private String cpf;
+
+    @NotNull(message = "O campo código é obrigatório")
+    @Column(unique = true, nullable = false)
+    private Long codigoUnico;
+
+    @NotBlank
+    private String cargo;
+
+    
 }
