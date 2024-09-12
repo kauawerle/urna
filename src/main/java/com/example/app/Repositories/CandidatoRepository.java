6848f8a722405da5b3fc35883e53c4dd980239f5
kauawerle <kauawerle19@gmail.com>
2024-09-12 19:50:00 -0300

    implement repositories in code

---
+++ src/main/java/com/example/app/Repositories/CandidatoRepository.java
@@ -0,0 +1,8 @@
+package com.example.app.Repositories;
+
+import com.example.app.Entities.Candidato;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CandidatoRepository extends JpaRepository<Candidato, Long> {
+
+}
---
+++ src/main/java/com/example/app/Repositories/VotoRepository.java
@@ -0,0 +1,7 @@
+package com.example.app.Repositories;
+
+import com.example.app.Entities.Voto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VotoRepository extends JpaRepository<Voto, Long> {
+}
